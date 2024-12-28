7a28f52a59cb77cad32b398325b5271e9ec7c20c
Egor Starygin <EgorStar555@gmail.com>
2024-12-28 13:25:31 +0400

    feat: gameOver added

--- app/src/main/java/com/example/guessinggame/GameFragment.kt
+++ app/src/main/java/com/example/guessinggame/GameFragment.kt
@@ -32,17 +32,18 @@ class GameFragment : Fragment() {
         viewModel.secretWordDisplay.observe(viewLifecycleOwner, Observer { newValue ->
             binding.word.text = newValue
         })
-
-        binding.guessButton.setOnClickListener() {
-            viewModel.makeGuess(binding.guess.text.toString().uppercase())
-            binding.guess.text = null
-            if (viewModel.isWon() || viewModel.isLost()) {
+        viewModel.gameOver.observe(viewLifecycleOwner, Observer { newValue ->
+            if (newValue) {
                 val action = GameFragmentDirections
                     .actionGameFragmentToResultFragment(viewModel.wonLostMessage())
                 view.findNavController().navigate(action)
             }
-        }
+        })
 
+        binding.guessButton.setOnClickListener() {
+            viewModel.makeGuess(binding.guess.text.toString().uppercase())
+            binding.guess.text = null
+        }
         return view
     }
 
--- app/src/main/java/com/example/guessinggame/GameViewModel.kt
+++ app/src/main/java/com/example/guessinggame/GameViewModel.kt
@@ -23,6 +23,10 @@ class GameViewModel : ViewModel() {
     val livesLeft: LiveData<Int>
         get() = _livesLeft
 
+    private val _gameOver = MutableLiveData<Boolean>(false)
+    val gameOver: LiveData<Boolean>
+        get() = _gameOver
+
     init {
         _secretWordDisplay.value = deriveSecretWordDisplay()
         Log.i("GameViewModel", "ViewModel init")
@@ -56,12 +60,13 @@ class GameViewModel : ViewModel() {
                 _incorrectGuesses.value += "$guess "
                 _livesLeft.value = _livesLeft.value?.minus(1)
             }
+            if (isWon() || isLost()) _gameOver.value = true
         }
     }
 
-    fun  isWon() = secretWord.equals(secretWordDisplay.value, true)
+    private fun  isWon() = secretWord.equals(secretWordDisplay.value, true)
 
-    fun isLost() = (livesLeft.value ?: 0) <= 0
+    private fun isLost() = (livesLeft.value ?: 0) <= 0
 
     fun wonLostMessage() : String {
         var message = ""
